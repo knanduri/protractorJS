9e44e576183a531a231052c920c7396c64d0bbbb
kishore <kishore.nanduri@qantasassure.com>
2018-03-02 16:03:55 +1100

    Updated the framework
     - Created a basic mocha test framework with imports and async awaits

--- spec.js
+++ spec.js
@@ -1,37 +1,11 @@
 import { expect } from "chai";
-import { ElementFinder, browser, by, element } from 'protractor';
+import { browser, by, element } from 'protractor';
 
 // spec.js
-describe('Protractor Demo App', function() {
-    const firstNumber = element(by.model('first'));
-    const secondNumber = element(by.model('second'));
-    const goButton = element(by.id('gobutton'));
-    const latestResult = element(by.binding('latest'));
-
-    beforeEach(function() {
-        browser.get("http://juliemr.github.io/protractor-demo/");
-    });
-
-    it('should have a title', function() {
-        expect(browser.getTitle()).toEqual('Super Calculator');
-    });
-
-    it('should add one and two', function() {
-        firstNumber.sendKeys(1);
-        secondNumber.sendKeys(2);
-
-        goButton.click();
-
-        expect(latestResult.getText()).toEqual('3');
-    });
-
-    it('should add four and six', function() {
-        // Fill this in.
-        expect(latestResult.getText()).toEqual('10');
-    });
-
-    it('should read the value from an input', function() {
-        firstNumber.sendKeys(1);
-        expect(firstNumber.getAttribute('value')).toEqual('1');
+describe('Protractor Demo App', async () => {
+    it('should have a title', async () => {
+        await browser.get('http://juliemr.github.io/protractor-demo/');
+        const title = await browser.getTitle();
+        await expect(title).equal('Super Calculator');
     });
 });
