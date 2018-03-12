46deca47d136552f30de01fb530aebc1d06ac162
kishore <kishore.nanduri@qantasassure.com>
2018-03-12 15:51:52 +1100

    - Updated the page object Model structure for the framework  - Used Expected Conditions of Protractor  - Updated package Json

--- featureTests/landingPage.test.js
+++ featureTests/landingPage.test.js
@@ -1,21 +1,17 @@
-import { expect } from "chai";
-import { browser, by, element } from 'protractor';
+import { browser } from 'protractor';
 import * as landingPage from "../helperFunctions/landingPage";
 
 before(async () => {
     await browser.get('http://bws.com.au/');
+    await browser.waitForAngular();
 });
-
 
 describe('Add an item to the cart', async () => {
     it('Should Click on the item successfully', async () => {
-        await browser.sleep(3000);
         await landingPage.selectProduct();
     });
 
     it('Verify item successfully added to cart', async () => {
-        await browser.sleep(3000);
-        const cartCount = await element(by.css('.shopping_cart_desktop')).getText();
-        await expect(cartCount).equal('1');
+        await landingPage.verifyCartCount();
     });
 });
--- helperFunctions/landingPage.js
+++ helperFunctions/landingPage.js
@@ -1,12 +1,18 @@
-import {by, element} from "protractor/built/index";
+import {by, element, ExpectedConditions} from "protractor";
+import { expect } from 'chai';
 
-class LoginPage {
+export const selectors = {
+    itemCode: element.all(by.css('[ng-click="ctrl.addToCart()"]')).get(1),
+    cartCount: element(by.css('.shopping_cart_desktop')),
+};
 
-       itemCode = element.all(by.css('[ng-click="ctrl.addToCart()"]')).get(1);
+export const selectProduct = async () => {
+    await ExpectedConditions.elementToBeClickable(selectors.itemCode);
+    await selectors.itemCode.click();
+};
 
-       selectProduct = async () => {
-            await this.itemCode.click();
-        };
-}
-
-export default LoginPage;
+export const verifyCartCount = async () => {
+    await ExpectedConditions.visibilityOf(selectors.cartCount);
+    const getCartCount = await selectors.cartCount.getText();
+    expect(getCartCount).to.equal('1');
+};
